31e6f069674b97647f20ca66c043bd9378d4476d
SyntaxStacks <johnbobsexipants@gmail.com>
2014-02-16 13:03:16 -0600

    Update orderSteps.py
    replaced return with pass where suitable.

--- tests/features/order/orderSteps.py
+++ tests/features/order/orderSteps.py
@@ -4,28 +4,29 @@ from lettuce import *
 @step('I have a valid order')
 def valid_order(step):
   #create a valid order
-  return
+  pass
 
 @step('I submit the order')
 def submit_order(step):
   #submit the order
-  return
+  pass
 @step('I should have created an order')
 def check_order(step):
   #validate order
-  return
+  pass
 
 #Creating Orders 
 @step('I have a valid updated order')
 def valid_updated_order(step):
   #create a valid order
-  return
+  pass
 
 @step('I submit the updated order')
 def submit_updated_order(step):
   #submit the order
-  return
+  pass
+
 @step('I should have updated the order')
 def check_updated_order(step):
   #validate order
-  return
+  pass
